a28bf1aa5610c3e385afebf0cf0eae4e213f8f3b
jschepens <jeremy.schepens@orange.fr>
2019-09-10 12:06:28 +0200

    Exercice 5

--- exercice 5/script5.js
+++ exercice 5/script5.js
@@ -1,5 +1,25 @@
-var imgElements = document.getElementsByClassName("conteneur");
+//var imgElements = document.getElementsByClassName("conteneur");
+var img1Element = document.getElementById("image1");
+var img2Element = document.getElementById("image2");
+var img3Element = document.getElementById("image3");
+var img4Element = document.getElementById("image4");
+var img5Element = document.getElementById("image5");
 
-var mouseOver = imgElements.addEventListener("mouseover", function(event){
+var mouseOver = img1Element.addEventListener("mouseover", function(event){
+    for (4):
+    
     event.target.src = "images/image1_2.jpg";
+
+});
+var mouseOver = img2Element.addEventListener("mouseover", function(event){
+    event.target.src = "images/image2_2.jpg";
+});
+var mouseOver = img3Element.addEventListener("mouseover", function(event){
+    event.target.src = "images/image3_2.jpg";
+});
+var mouseOver = img4Element.addEventListener("mouseover", function(event){
+    event.target.src = "images/image4_2.jpg";
+});
+var mouseOver = img5Element.addEventListener("mouseover", function(event){
+    event.target.src = "images/image5_2.jpg";
 });
