d5b06fa13b22229184fda6affb0fb963430469fb
dadurand <ha.halo@live.fr>
2019-09-10 12:31:50 +0200

    Exercice 5

--- exercice 5/script5.js
+++ exercice 5/script5.js
@@ -1,25 +1,38 @@
-//var imgElements = document.getElementsByClassName("conteneur");
+var imgElements = document.getElementsByClassName("conteneur");
 var img1Element = document.getElementById("image1");
 var img2Element = document.getElementById("image2");
 var img3Element = document.getElementById("image3");
 var img4Element = document.getElementById("image4");
 var img5Element = document.getElementById("image5");
 
-var mouseOver = img1Element.addEventListener("mouseover", function(event){
-    for (4):
-    
-    event.target.src = "images/image1_2.jpg";
 
-});
-var mouseOver = img2Element.addEventListener("mouseover", function(event){
+var mouseOver = imgElements.addEventListener("mouseover", function(event){
+    alert("test")
     event.target.src = "images/image2_2.jpg";
-});
-var mouseOver = img3Element.addEventListener("mouseover", function(event){
-    event.target.src = "images/image3_2.jpg";
-});
-var mouseOver = img4Element.addEventListener("mouseover", function(event){
-    event.target.src = "images/image4_2.jpg";
-});
-var mouseOver = img5Element.addEventListener("mouseover", function(event){
-    event.target.src = "images/image5_2.jpg";
-});
+
+}
+
+
+// if (img1Element.addEventListener("mouseover", function (event)){
+//     for (let pas = 0; pas < 4; pas++) {
+//         if (event.target.src != "images/image" + str(pas) + "_2.jpg")
+//             event.target.src = "images/image" + str(pas) + "_2.jpg";
+//     };
+
+// }
+
+
+
+
+
+// });
+// });
+// var mouseOver = img3Element.addEventListener("mouseover", function(event){
+//     event.target.src = "images/image3_2.jpg";
+// });
+// var mouseOver = img4Element.addEventListener("mouseover", function(event){
+//     event.target.src = "images/image4_2.jpg";
+// });
+// var mouseOver = img5Element.addEventListener("mouseover", function(event){
+//     event.target.src = "images/image5_2.jpg";
+// });
