7f6846a6a92109287af5dae6aeb936171d33d242
dadurand <ha.halo@live.fr>
2019-09-10 11:31:27 +0200

    Exercice 4

--- exercice 4/script4.js
+++ exercice 4/script4.js
@@ -1,3 +1,10 @@
-var btnElement = document.getElementsByTagName("button");
+var btnElement = document.body.childNodes[1][3];
+var firstNameElement = document.getElementById("nom") ;
+var lastNameElement = document.getElementById("prenom") ;
+var cityElement = document.getElementById("ville") ;
 
-var resetInput = btnElement.addEventListener
+var reset = btnElement.addEventListener("click", function(){
+    firstNameElement.value = "";
+    lastNameElement.value = "";
+    cityElement.value = "";
+})
